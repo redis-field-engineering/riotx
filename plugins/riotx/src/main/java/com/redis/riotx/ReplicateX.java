878c994cb8d50a198722a299bd43609d0f8daec1
jruaux <jruaux@gmail.com>
2025-02-17 15:54:38 -0800

    feat: Added `--remove-source-keys` option to replicate command

--- plugins/riotx/src/main/java/com/redis/riotx/ReplicateX.java
+++ plugins/riotx/src/main/java/com/redis/riotx/ReplicateX.java
@@ -2,6 +2,8 @@ package com.redis.riotx;
 
 import java.io.IOException;
 
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.CompositeItemWriter;
 import org.springframework.util.StringUtils;
 
 import com.redis.riot.RedisContext;
@@ -12,15 +14,21 @@ import com.redis.spring.batch.item.redis.RedisItemReader;
 import com.redis.spring.batch.item.redis.RedisItemWriter;
 import com.redis.spring.batch.item.redis.common.KeyValue;
 import com.redis.spring.batch.item.redis.common.RedisInfo;
+import com.redis.spring.batch.item.redis.writer.impl.Del;
 
+import io.lettuce.core.codec.ByteArrayCodec;
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 @Command(name = "replicate", aliases = "sync", description = "Replicate a Redis database into another Redis database.")
 public class ReplicateX extends Replicate {
 
 	@ArgGroup(exclusive = false, heading = "Metrics options%n")
 	private MetricsArgs metricsArgs = new MetricsArgs();
+
+	@Option(names = "--remove-source-keys", description = "Delete keys from source after they have been successfully replicated.")
+	private boolean removeSourceKeys;
 
 	@Override
 	protected void initialize() {
@@ -37,6 +45,19 @@ public class ReplicateX extends Replicate {
 		RedisItemReader<byte[], byte[]> reader = super.reader();
 		reader.addItemReadListener(new ReplicateMetricsReadListener<>());
 		return reader;
+	}
+
+	@Override
+	protected ItemWriter<KeyValue<byte[]>> replicateWriter() {
+		ItemWriter<KeyValue<byte[]>> writer = super.replicateWriter();
+		if (removeSourceKeys) {
+			log.info("Adding source delete writer to replicate writer");
+			RedisItemWriter<byte[], byte[], KeyValue<byte[]>> sourceDelete = new RedisItemWriter<>(
+					ByteArrayCodec.INSTANCE, new Del<>(KeyValue::getKey));
+			configureSourceRedisWriter(sourceDelete);
+			return new CompositeItemWriter<>(writer, sourceDelete);
+		}
+		return writer;
 	}
 
 	@Override
@@ -57,8 +78,8 @@ public class ReplicateX extends Replicate {
 	}
 
 	@Override
-	protected Step<KeyValue<byte[]>, KeyValue<byte[]>> step() {
-		Step<KeyValue<byte[]>, KeyValue<byte[]>> step = super.step();
+	protected Step<KeyValue<byte[]>, KeyValue<byte[]>> replicateStep() {
+		Step<KeyValue<byte[]>, KeyValue<byte[]>> step = super.replicateStep();
 		step.writeListener(new ReplicateMetricsWriteListener<>());
 		return step;
 	}
