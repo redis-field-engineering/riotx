6bad1895cb03633821b21f66e218e26b06c49fdb
Julien Ruaux <jruaux@gmail.com>
2025-04-14 23:44:47 -0700

    test: Properly shut down client

--- core/spring-batch-redis-test/src/test/java/com/redis/spring/batch/test/ValkeyTests.java
+++ core/spring-batch-redis-test/src/test/java/com/redis/spring/batch/test/ValkeyTests.java
@@ -22,30 +22,31 @@ import io.lettuce.core.RedisURI;
 @TestInstance(Lifecycle.PER_CLASS)
 public class ValkeyTests {
 
-	@SuppressWarnings("resource")
-	private GenericContainer<?> valkey = new GenericContainer<>("valkey/valkey").withExposedPorts(6379);
+    @SuppressWarnings("resource")
+    private GenericContainer<?> valkey = new GenericContainer<>("valkey/valkey").withExposedPorts(6379);
 
-	protected final Logger log = LoggerFactory.getLogger(getClass());
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 
-	@BeforeAll
-	void setup() throws Exception {
-		valkey.start();
-	}
+    @BeforeAll
+    void setup() throws Exception {
+        valkey.start();
+    }
 
-	@AfterAll
-	void teardown() {
-		valkey.stop();
-	}
+    @AfterAll
+    void teardown() {
+        valkey.stop();
+    }
 
-	@Test
-	void testRedisSupport() {
-		RedisModulesClient client = RedisModulesClient
-				.create(RedisURI.create(valkey.getHost(), valkey.getFirstMappedPort()));
-		RedisSupportCheck check = new RedisSupportCheck();
-		check.getConsumers().clear();
-		List<RedisInfo> infoList = new ArrayList<>();
-		check.getConsumers().add(infoList::add);
-		check.accept(client);
-		Assertions.assertEquals(1, infoList.size());
-	}
+    @Test
+    void testRedisSupport() {
+        RedisModulesClient client = RedisModulesClient.create(RedisURI.create(valkey.getHost(), valkey.getFirstMappedPort()));
+        RedisSupportCheck check = new RedisSupportCheck();
+        check.getConsumers().clear();
+        List<RedisInfo> infoList = new ArrayList<>();
+        check.getConsumers().add(infoList::add);
+        check.accept(client);
+        Assertions.assertEquals(1, infoList.size());
+        client.shutdown();
+    }
+
 }
