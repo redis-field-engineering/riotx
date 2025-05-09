67468eb92eb5eb813e47807a93e14dac5639f894
Julien Ruaux <jruaux@gmail.com>
2025-05-08 23:07:57 -0700

    style: Removed unnecessary throws declaration

--- plugins/riotx/src/main/java/com/redis/riot/Versions.java
+++ plugins/riotx/src/main/java/com/redis/riot/Versions.java
@@ -8,10 +8,11 @@ import picocli.CommandLine.IVersionProvider;
 
 public class Versions implements IVersionProvider {
 
-	@Override
-	public String[] getVersion() throws Exception {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		RiotVersion.banner(RiotUtils.newPrintStream(baos));
-		return RiotUtils.toString(baos).split(System.lineSeparator());
-	}
+    @Override
+    public String[] getVersion() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        RiotVersion.banner(RiotUtils.newPrintStream(baos));
+        return RiotUtils.toString(baos).split(System.lineSeparator());
+    }
+
 }
