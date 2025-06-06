bfc574475862c373f99355a2529faa8164b50657
Julien Ruaux <jruaux@gmail.com>
2025-06-06 15:38:33 -0700

    test: Fixed memcached tests

--- spring-batch/spring-batch-memcached/src/test/java/com/redis/spring/batch/memcached/MemcachedTests.java
+++ spring-batch/spring-batch-memcached/src/test/java/com/redis/spring/batch/memcached/MemcachedTests.java
@@ -1,17 +1,5 @@
 package com.redis.spring.batch.memcached;
 
-import com.redis.testcontainers.MemcachedContainer;
-import net.spy.memcached.MemcachedClient;
-import net.spy.memcached.transcoders.Transcoder;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.springframework.batch.item.Chunk;
-import org.springframework.batch.item.ExecutionContext;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.file.transform.Range;
-import org.springframework.util.ClassUtils;
-import org.testcontainers.utility.DockerImageName;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
@@ -21,20 +9,38 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.util.ClassUtils;
+import org.testcontainers.utility.DockerImageName;
+
+import com.redis.testcontainers.MemcachedContainer;
+
+import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.transcoders.Transcoder;
+
 @TestInstance(Lifecycle.PER_CLASS)
 public class MemcachedTests {
 
-    private static final DockerImageName imageName = MemcachedContainer.DEFAULT_IMAGE_NAME.withTag(
-            MemcachedContainer.DEFAULT_TAG);
+    private static final DockerImageName imageName = MemcachedContainer.DEFAULT_IMAGE_NAME
+            .withTag(MemcachedContainer.DEFAULT_TAG);
 
     private static final MemcachedContainer source = new MemcachedContainer(imageName);
-
     private static final MemcachedContainer target = new MemcachedContainer(imageName);
 
     private Supplier<MemcachedClient> clientSupplier;
-
     private MemcachedClient client;
-
+    private Supplier<MemcachedClient> targetClientSupplier;
     private MemcachedClient targetClient;
 
     private static final Transcoder<byte[]> transcoder = new ByteArrayTranscoder();
@@ -60,7 +66,8 @@ public class MemcachedTests {
         target.start();
         clientSupplier = () -> client(source.getMemcachedAddresses());
         client = clientSupplier.get();
-        targetClient = client(target.getMemcachedAddresses());
+        targetClientSupplier = () -> client(target.getMemcachedAddresses());
+        targetClient = targetClientSupplier.get();
     }
 
     private static MemcachedClient client(List<InetSocketAddress> addresses) {
@@ -76,11 +83,15 @@ public class MemcachedTests {
         if (client != null) {
             client.shutdown();
         }
-        source.stop();
+        if (source != null) {
+            source.stop();
+        }
         if (targetClient != null) {
             targetClient.shutdown();
         }
-        target.stop();
+        if (target != null) {
+            target.stop();
+        }
     }
 
     @BeforeEach
@@ -102,6 +113,7 @@ public class MemcachedTests {
             MemcachedEntry actual = new MemcachedEntry();
             actual.setKey(entry.getKey());
             actual.setValue(client.get(entry.getKey(), transcoder));
+            actual.setExpiration(-1);
             assertEquals(entry, actual);
         }
     }
@@ -162,8 +174,8 @@ public class MemcachedTests {
     private static void assertEquals(MemcachedEntry expected, MemcachedEntry actual) {
         Assertions.assertEquals(expected.getKey(), actual.getKey());
         Assertions.assertArrayEquals(expected.getValue(), actual.getValue());
-        if (expected.getExpiration() == null) {
-            Assertions.assertNull(actual.getExpiration());
+        if (expected.getExpiration() == 0) {
+            Assertions.assertEquals(-1, actual.getExpiration());
         } else {
             Assertions.assertEquals(expected.getExpiration(), actual.getExpiration());
         }
