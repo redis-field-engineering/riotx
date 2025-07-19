227d8d9d0fb5dedc71a9e43c9c0636f8bd167608
Julien Ruaux <jruaux@gmail.com>
2025-07-18 17:21:13 -0700

    feat: Added Redis commands `search` and `aggregate`

--- plugins/riotx/src/main/java/com/redis/riot/operation/AggregateCommand.java
+++ plugins/riotx/src/main/java/com/redis/riot/operation/AggregateCommand.java
@@ -2,9 +2,8 @@ package com.redis.riot.operation;
 
 import com.redis.batch.BatchUtils;
 import com.redis.batch.RedisBatchOperation;
+import com.redis.batch.operation.Aggregate;
 import com.redis.batch.operation.Search;
-import com.redis.lettucemod.search.Limit;
-import com.redis.lettucemod.search.SearchOptions;
 import com.redis.riot.BaseCommand;
 import com.redis.riot.core.TemplateExpression;
 import org.springframework.expression.EvaluationContext;
@@ -17,8 +16,8 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-@Command(name = "search", description = "Execute FT.SEARCH queries")
-public class SearchCommand extends BaseCommand implements OperationCommand {
+@Command(name = "aggregate", description = "Execute FT.AGGREGATE aggregations")
+public class AggregateCommand extends BaseCommand implements OperationCommand {
 
     @CommandLine.Parameters(arity = "1", index = "0", description = "Index template expression.", paramLabel = "INDEX")
     private TemplateExpression index;
@@ -26,18 +25,18 @@ public class SearchCommand extends BaseCommand implements OperationCommand {
     @CommandLine.Parameters(arity = "1", index = "1", description = "Query template expression.", paramLabel = "QUERY")
     private TemplateExpression query;
 
-    @CommandLine.Parameters(arity = "0..*", index = "2..*", description = "Search options expressions, e.g. limit 0 10 withpayloads.", paramLabel = "OPTIONS")
+    @CommandLine.Parameters(arity = "0..*", index = "2..*", description = "Aggregate options expressions, e.g. groupby 1 @category reduce avg 1 @score as score sortby 2 @score desc.", paramLabel = "OPTIONS")
     private List<TemplateExpression> options = new ArrayList<>();
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
     public RedisBatchOperation<byte[], byte[], Map<String, Object>, Object> operation(EvaluationContext context) {
-        Search<byte[], byte[], Map<String, Object>> search = new Search<>(
+        Aggregate<byte[], byte[], Map<String, Object>> aggregate = new Aggregate<>(
                 index(context).andThen(BatchUtils.STRING_KEY_TO_BYTES),
                 query(context).andThen(BatchUtils.STRING_VALUE_TO_BYTES));
-        search.setStringOptions(t -> options.stream().map(e -> e.getValue(context, t)).map(BatchUtils.STRING_VALUE_TO_BYTES)
+        aggregate.setStringOptions(t -> options.stream().map(e -> e.getValue(context, t)).map(BatchUtils.STRING_VALUE_TO_BYTES)
                 .collect(Collectors.toList()));
-        return (RedisBatchOperation) search;
+        return (RedisBatchOperation) aggregate;
     }
 
     private Function<Map<String, Object>, String> index(EvaluationContext context) {
